703efcaa7f93a9c5bd47c30351804d7b220cdc6e
natalia_yepifanova <epif97@mail.ru>
2025-03-03 09:42:17 +0300

    fix hh_api_request

--- src/hh_api_request.py
+++ src/hh_api_request.py
@@ -4,13 +4,19 @@ import requests
 class HeadHunterAPI:
     base_url: str
     vacancies_url: str
+    vacancies_list: list
+    employer_info: dict
+    employers_info_list: list
 
     def __init__(self, url="https://api.hh.ru"):
         """Инициализация класса"""
         self.base_url = url
+        self.vacancies_list = []
+        self.employer_info = {}
+        self.employers_info_list = []
 
     def get_employer_id_by_name(self, employer_name):
-        """Функция для получения id работодателя по названию компании и получения ссылки на вакансии"""
+        """Функция для получения id работодателя по названию компании"""
         url = f"{self.base_url}/employers"
         params = {"text": employer_name}
         response = requests.get(url, params=params)
@@ -18,13 +24,30 @@ class HeadHunterAPI:
             employers = response.json().get("items", [])
             for employer in employers:
                 if employer["name"] == employer_name:
-                    self.vacancies_url = employer["vacancies_url"]
+                    self.vacancies_url = employer['vacancies_url']
                     return employer["id"]
             else:
                 print(f"Работодатель с названием '{employer_name}' не найден.")
                 return None
         else:
             print(f"Ошибка при получении ID работодателя: {response.status_code}")
+            return None
+
+    def get_employer_info_by_id(self, employer_id):
+        """Функция для получения информации о работодателе по id"""
+        response = requests.get(f"{self.base_url}/employers/{employer_id}")
+        if response.status_code == 200:
+            employer = response.json()
+            employer_info = {
+                'name': employer["name"],
+                'id': employer["id"],
+                'vacancies_url': employer['vacancies_url'],
+                'site_url': employer['site_url'],
+                'description_url': employer['alternate_url']
+            }
+            return employer_info
+        else:
+            print(f"Ошибка при получении информации о работодателе: {response.status_code}")
             return None
 
     def get_employer_vacancies(self):
@@ -36,42 +59,23 @@ class HeadHunterAPI:
             print(f"Ошибка при получении вакансий: {response.status_code}")
             return []
 
-    #
-    #     if response.status_code == 200:
-    #         return response.json().get('items', [])
+    def fetch_vacancies_for_employers(self, employer_names):
+        """Получает вакансии для списка работодателей по их названиям."""
+        for employer_name in employer_names:
+            employer_id = self.get_employer_id_by_name(employer_name)
+            if employer_id:
+                self.vacancies_list.append(self.get_employer_vacancies())
 
-    #
-    # def fetch_vacancies_for_employers(self, employer_names):
-    #     """Получает вакансии для списка работодателей по их названиям."""
-    #     for employer_name in employer_names:
-    #         employer_id = self.get_employer_id(employer_name)
-    #         if employer_id:
-    #             vacancies = self.get_employer_vacancies(employer_id)
-    #             print(f"Вакансии для работодателя '{employer_name}':")
-    #             for vac in vacancies:
-    #                 salary = vac.get('salary', {})
-    #                 salary_from = salary.get('from', 'не указана')
-    #                 salary_to = salary.get('to', 'не указана')
-    #                 print(f"- {vac['name']} (Зарплата: {salary_from} - {salary_to})")
-    #             print("\n")  # Пустая строка для разделения между работодателями
+        return self.vacancies_list
 
+    def fetch_info_for_employers(self, employer_names):
+        """Получает информацию о всех работодателях из списка по их названиям."""
+        for employer_name in employer_names:
+            employer_id = self.get_employer_id_by_name(employer_name)
+            if employer_id:
+                employer_info = self.get_employer_info_by_id(employer_id)
+                if employer_info:
+                    self.employers_info_list.append(employer_info)
 
-# if __name__ == "__main__":
-#     # Список названий работодателей, от которых вы хотите получать данные о вакансиях
-#     employer_names = [
-#         "Яндекс",
-#         "Сбер",
-#         "Тинькофф",
-#         "Mail.ru Group",
-#         "Газпром",
-#         "РТС",
-#         "Альфа-Банк",
-#         "Ростелеком",
-#         "МТС",
-#         "ВТБ"
-#     ]
-#
-#     hh_api = HeadHunterAPI()
-#     hh_api.get_employer_id_by_name('Ростелеком')
-#     print(hh_api.get_employer_vacancies())
-#  #   hh_api.fetch_vacancies_for_employers(employer_names)
+        return self.employers_info_list
+
